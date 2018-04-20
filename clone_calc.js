0c6e9be2e3ac8c1d3fa5a1b5aa56cad2c1a988e3
genie45 <genie45bot@gmail.com>
2018-04-20 02:34:00 +0200

    added basic way of calculation need to fix errors

--- clone_calc.js
+++ clone_calc.js
@@ -1,2 +1,21 @@
 const CloneElementCostPerLevelGlobalMultiplier = 5000;
 const CloneBaseElementCostGlobalMultiplier = 2500;
+
+var CharacterLevel = document.getElementById("CharacterLevel");
+var CloneElementCostPerLevel = document.getElementById("CloneElementCostPerLevel");
+var CloneBaseElementCost = document.getElementById("CloneBaseElementCost");
+var CloneTimePerElementShard = document.getElementById("CloneTimePerElementShard");
+var BabyMatureSpeedMultiplier = document.getElementById("BabyMatureSpeedMultiplier");
+var CloneCostResult = 0;
+var CloneTimeResult = 0;
+
+function StartCalculate() {
+    var x = CloneElementCostPerLevel * CloneElementCostPerLevelGlobalMultiplier; console.log(x);
+    var y = x * CharacterLevel; console.log(y);
+    var z = CloneBaseElementCostGlobalMultiplier * CloneBaseElementCost; console.log(z);
+    CloneCostResult = Math.round(y + z); console.log(CloneCostResult);
+    var a = CloneTimePerElementShard / BabyMatureSpeedMultiplier; console.log(a);
+    CloneTimeResult = CloneCostResult * a; console.log(CloneTimeResult);
+    document.getElementById("CloneCostResult").innerHTML = CloneCostResult;
+    document.getElementById("CloneTimeResult").innerHTML = CloneTimeResult;
+}
