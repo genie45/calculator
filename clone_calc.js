4b7dd29d8e604f16d5a0d684ed52d731d9cca084
genie45 <genie45bot@gmail.com>
2018-04-20 12:25:51 +0200

    messing around and breaking more stuff

--- clone_calc.js
+++ clone_calc.js
@@ -1,21 +1,21 @@
 const CloneElementCostPerLevelGlobalMultiplier = 5000;
 const CloneBaseElementCostGlobalMultiplier = 2500;
 
-var CharacterLevel = document.getElementById("CharacterLevel");
-var CloneElementCostPerLevel = document.getElementById("CloneElementCostPerLevel");
-var CloneBaseElementCost = document.getElementById("CloneBaseElementCost");
-var CloneTimePerElementShard = document.getElementById("CloneTimePerElementShard");
-var BabyMatureSpeedMultiplier = document.getElementById("BabyMatureSpeedMultiplier");
 var CloneCostResult = 0;
 var CloneTimeResult = 0;
 
 function StartCalculate() {
-    var x = CloneElementCostPerLevel * CloneElementCostPerLevelGlobalMultiplier; console.log(x);
-    var y = x * CharacterLevel; console.log(y);
-    var z = CloneBaseElementCostGlobalMultiplier * CloneBaseElementCost; console.log(z);
-    CloneCostResult = Math.round(y + z); console.log(CloneCostResult);
-    var a = CloneTimePerElementShard / BabyMatureSpeedMultiplier; console.log(a);
-    CloneTimeResult = CloneCostResult * a; console.log(CloneTimeResult);
+    var CharacterLevel = document.getElementById("CharacterLevel"); console.log('CharacterLevel: ' + CharacterLevel);
+    var CloneElementCostPerLevel = document.getElementById("CloneElementCostPerLevel"); console.log('CloneElementCostPerLevel: ' + CloneElementCostPerLevel);
+    var CloneBaseElementCost = document.getElementById("CloneBaseElementCost"); console.log('CloneBaseElementCost: ' + CloneBaseElementCost);
+    var CloneTimePerElementShard = document.getElementById("CloneTimePerElementShard"); console.log('CloneTimePerElementShard: ' + CloneTimePerElementShard);
+    var BabyMatureSpeedMultiplier = document.getElementById("BabyMatureSpeedMultiplier"); console.log('BabyMatureSpeedMultiplier: ' + BabyMatureSpeedMultiplier);
+    var x = CloneElementCostPerLevel * CloneElementCostPerLevelGlobalMultiplier; console.log('firststep: ' + x);
+    var y = x * CharacterLevel; console.log('second step: ' + y);
+    var z = CloneBaseElementCostGlobalMultiplier * CloneBaseElementCost; console.log('third step: ' + z);
+    CloneCostResult = Math.round(y + z); console.log('CloneCostResult: ' + CloneCostResult);
+    var a = CloneTimePerElementShard / BabyMatureSpeedMultiplier; console.log('fourth step: ' + a);
+    CloneTimeResult = CloneCostResult * a; console.log('CloneTimeResult: ' + CloneTimeResult);
     document.getElementById("CloneCostResult").innerHTML = CloneCostResult;
     document.getElementById("CloneTimeResult").innerHTML = CloneTimeResult;
 }
