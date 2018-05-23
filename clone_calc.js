7ed7a685827e609a8ddc8965856266d8d71d5dde
genie45 <genie45bot@gmail.com>
2018-05-23 17:31:50 +0200

    tecchnicly done just need to find the errors

--- clone_calc.js
+++ clone_calc.js
@@ -1,18 +1,14 @@
-const CloneElementCostPerLevelGlobalMultiplier = 5000; //fixed value from Cloning Chamber
-const CloneBaseElementCostGlobalMultiplier = 2500; //fixed value from Cloning Chamber
-
-var CloningTimePerElementShard = 7; //fixed value from Cloning Chamber
+var stats = null;
+var wildStatCalcDiv = null;
 var CloneCostResult = 0;
 var CloneTimeResult = 0;
-var CloneValuesDiv = null;
-var CreatureStats = null;
-var CloneValues ={
-    'CloneElementCostPerLevel': 'CloneElementCostPerLevel',
-    'CloneBaseElementCost': 'CloneBaseElementCost',
-};
-
-if (CloneValuesDiv = document.getElementById('CloneCalc')){
-    CreatureStats = {
+var BabyMatureSpeedMultiplier = 0;
+var CharacterLevel = 0;
+const CloneElementCostPerLevelGlobalMultiplier = 5000; //fixed value from Cloning Chamber
+const CloneBaseElementCostGlobalMultiplier = 2500; //fixed value from Cloning Chamber
+const CloningTimePerElementShard = 7; //fixed value from Cloning Chamber
+if (wildStatCalcDiv = document.getElementById('')) {
+    var CloningValuesDinos = {
         "Achatina": {
             "CloneElementCostPerLevel": 0.01,
             "CloneBaseElementCost": 0.69,
@@ -433,31 +429,46 @@ if (CloneValuesDiv = document.getElementById('CloneCalc')){
             "CloneElementCostPerLevel": 0.01,
             "CloneBaseElementCost": 0.45,
         },
-    }
-}
-
-changeCreature(CloneValuesDiv.innerHTML);
+    };
+    changeCreature(wildStatCalcDiv.innerHTML);
+};
 
 function changeCreature(creature) {
-    if (CreatureStats[creature] == null)
-        creature = Object.keys(CreatureStats)[0];
+    if (CloningValuesDinos[creature] == null)
+        creature = Object.keys(CloningValuesDinos)[0];
+
+    stats = CloningValuesDinos[creature];
+    var tableRows = '';
+    tableRows += '<tr><td colspan="2">BabyMatureSpeedMultiplier</td><td colspan="2"><input type="number" min"0.1" max="100" step= "0.1" value="1.0" id="BabyMatureSpeedMultiplier" onchange="startCalculate()"></td></tr>';
+    tableRows += '<tr><td colspan="2">Level</td><td colspan="2"><input type="number" min="1" max="500" step="1" value="150" id="CharacterLevel" onchange="startCalculate()"></td></tr>';
+    tableRows += '<tr><td colspan="2">CloneElementCostPerLevel</td><td colspan="2">' + creature[0] + '</td></tr>';
+    tableRows += '<tr><td colspan="2">CloneBaseElementCost</td><td colspan="2">' + creature[1] + '</td></tr>';
+    tableRows += '<tr><td>Clone Cost</td><td>' + CloneCostResult + ' Element Shards</td><td>Clone Time</td><td>' + CloneTimeResult + ' seconds</td></tr>'
+    var creatureSelect = '<select onchange="changeCreature(this.value)">';
+    for (var c in CloningValuesDinos) {
+        creatureSelect += '<option' + (c === creature ? ' selected' : '') + '>' + c + '</option>';
+    }
+    creatureSelect += '</select>';
+    wildStatCalcDiv.innerHTML = '<table class="wikitable">' +
+      '<tr><th colspan="4">' + creatureSelect + 
+      tableRows +
+      '</table>';
+    startCalculate();
 }
 
+function startCalculate() {
+    //Clone Cost
+    var cost1 = 0;
+    var cost2 = 0;
+    var cost3 = 0;
+    cost1 = creature[0] * CloneElementCostPerLevelGlobalMultiplier;
+    cost2 = cost1 * document.getElementById('CharacterLevel'+ 'input').value;
+    cost3 = creature[1] * CloneBaseElementCostGlobalMultiplier;
+    CloneCostResult = Math.round(cost2 + cost3); console.log('CloneCostResult: ' + CloneCostResult);
+    //Clone Time
+    var time1 = 0;
+    time1 = CloningTimePerElementShard / document.getElementById('BabyMatureSpeedMultiplier' + 'input').value;
+    CloneTimeResult = Math.round(CloneCostResult * time1); console.log('CloneTimeResult: ' + CloneTimeResult);
 
-
-/*
-function StartCalculate() {
-    var CharacterLevel = document.getElementById("CharacterLevel"); console.log('CharacterLevel: ' + CharacterLevel);
-    var CloneElementCostPerLevel = document.getElementById("CloneElementCostPerLevel"); console.log('CloneElementCostPerLevel: ' + CloneElementCostPerLevel);
-    var CloneBaseElementCost = document.getElementById("CloneBaseElementCost"); console.log('CloneBaseElementCost: ' + CloneBaseElementCost);
-    var CloneTimePerElementShard = document.getElementById("CloneTimePerElementShard"); console.log('CloneTimePerElementShard: ' + CloneTimePerElementShard);
-    var BabyMatureSpeedMultiplier = document.getElementById("BabyMatureSpeedMultiplier"); console.log('BabyMatureSpeedMultiplier: ' + BabyMatureSpeedMultiplier);
-    var x = CloneElementCostPerLevel * CloneElementCostPerLevelGlobalMultiplier; console.log('firststep: ' + x);
-    var y = x * CharacterLevel; console.log('second step: ' + y);
-    var z = CloneBaseElementCostGlobalMultiplier * CloneBaseElementCost; console.log('third step: ' + z);
-    CloneCostResult = Math.round(y + z); console.log('CloneCostResult: ' + CloneCostResult);
-    var a = CloneTimePerElementShard / BabyMatureSpeedMultiplier; console.log('fourth step: ' + a);
-    CloneTimeResult = CloneCostResult * a; console.log('CloneTimeResult: ' + CloneTimeResult);
-    document.getElementById("CloneCostResult").innerHTML = CloneCostResult;
-    document.getElementById("CloneTimeResult").innerHTML = CloneTimeResult;
-}*/
+    changeCreature();
+}
